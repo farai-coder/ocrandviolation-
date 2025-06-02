7f24915bf5fa5a70f614d22de592bda0d37a88f8
TIINO07 <tinotendatakaendesa4@gmail.com>
2025-06-02 15:00:05 +0200

    added uvicorn

--- main.py
+++ main.py
@@ -36,3 +36,8 @@ app.mount(
     StaticFiles(directory=DIST_DIR, html=True),
     name="static",
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+        
